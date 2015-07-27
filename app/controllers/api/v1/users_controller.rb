46cb728fae7ffc278642e8dd96b2cf4178ac64c2
Morgan Miller <m.miller722@gmail.com>
2015-07-27 11:15:12 -0600

    add create to users api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,5 +8,14 @@ class Api::V1::UsersController < ApplicationController
   def show
     respond_with User.find_by(id: params[:id])
   end
-  
+
+  def create
+    respond_with User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
