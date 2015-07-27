68b488cc7aeee74ecf37773c63a718aa01266fb9
Morgan Miller <m.miller722@gmail.com>
2015-07-27 11:11:32 -0600

    add routes for users to api

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   resources :items
   resources :users
   resources :orders, only: [:index, :show]
-  
+
   namespace :api do
     namespace :v1 do
       resources :items, except: [:new, :edit]
+      resources :users, except: [:new, :edit]
     end
   end
 end
