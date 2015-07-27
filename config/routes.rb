b504f8b0b4edd899fda78a0d6e6b7fb9e511effd
Morgan Miller <m.miller722@gmail.com>
2015-07-27 10:40:43 -0600

    add item routes to v1 api

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources :items
   resources :users
   resources :orders, only: [:index, :show]
+  
+  namespace :api do
+    namespace :v1 do
+      resources :items, except: [:new, :edit]
+    end
+  end
 end
